9d88fb79f266cf0f8753c022a8236c16d5937ead
Cristiansitooo <66567463+Cristiansitooo@users.noreply.github.com>
2020-07-26 17:38:25 -0500

    Add files via upload

---
+++ build/precache-manifest.028a7e351127c60de65c789724b6bf57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa5b60e5162712c07c411838dc4cd11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc30f62d426b9f2937a0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c256f1ca1b3940d778c6",
+    "url": "/static/js/2.1aa283f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1aa283f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc30f62d426b9f2937a0",
+    "url": "/static/js/main.2f0871d7.chunk.js"
+  },
+  {
+    "revision": "261f31938efd99a09123",
+    "url": "/static/js/runtime-main.6d75ccfb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
